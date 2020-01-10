12f9f752e0677ea87b8e640cf1865aee4753d1c4
Frakir021 <JorakhMarmont@gmail.com>
2020-01-11 00:17:00 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <div className="vasya" style={{ border: '1px solid gray' }}>
+        <label htmlFor="name">Enter your name: </label>
+        <input type="text" id="name" />
+      </div>
+      <p>Some text here</p>
+    </Fragment>
   );
-}
+};
 
 export default App;
